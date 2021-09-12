981f40ef6803f1d77dedef9737f17cf9e422a2ff
ertugrulaktepe <66203249+ertugrulaktepe@users.noreply.github.com>
2021-09-12 13:37:33 +0300

    search bar eklendi

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "./style.css"
 function App() {
   const API_KEY = "5227242edce84edeb076e008d734cbc1"
   const API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`
+  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=`
   
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
   
   
   const getRequest = async ()=>{
@@ -19,16 +21,28 @@ function App() {
   useEffect(()=>{
     getRequest()
   },[])
+  const handleOnSubmit = async (e)=>{
+    e.preventDefault()
+    const response = await fetch(SEARCH_API + searchTerm)
+    const data = await response.json()
+    setMovies(data.results)
+
+  }
+  const handleOnChange = (e)=>{
+   setSearchTerm(e.target.value);
   
+  }
 
   return (
    <>
    <header>
-     <form >
+     <form onSubmit={handleOnSubmit}>
         <input 
         className="search" 
         type="search"
          placeholder="Search..." 
+         onChange={handleOnChange}
+         value={searchTerm}
          
          />
      </form>
